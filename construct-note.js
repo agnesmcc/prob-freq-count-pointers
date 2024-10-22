298a1b737c6d2f974c8466752cd947dd82a3947f
Agnes <agnes.mccall5@gmail.com>
2024-10-21 21:35:28 -0400

    add constructNote function

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,29 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+    let msg_freq = {};
+    let letters_freq = {};
+    let result = true;
+
+    if (letters.length == 0) {
+      return false;
+    }
+
+    for (let i = 0; i < msg.length; i++) {
+      msg_freq[msg[i]] = (msg_freq[msg[i]] || 0) + 1;
+    }
+
+    for (let i = 0; i < letters.length; i++) {
+      letters_freq[letters[i]] = (letters_freq[letters[i]] || 0) + 1;
+    }
+
+    Object.keys(msg_freq).forEach(key => {
+      if (msg_freq[key] > letters_freq[key]) {
+        result = false;
+      }
+    });
+
+
+    return result;
 }
+
+module.exports = constructNote;
