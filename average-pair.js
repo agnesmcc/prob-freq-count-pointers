78f772e962c658cd0468301217a1105d63d50acc
Agnes <agnes.mccall5@gmail.com>
2024-10-21 21:54:45 -0400

    add averagePair function

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, target_avg) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start < end) {
+        let avg = (nums[start] + nums[end]) / 2;
+        if (avg === target_avg) {
+            return true;
+        } else if (avg < target_avg) {
+            start++;
+        } else {
+            end--;
+        }
+    }
+
+    return false;
 }
+
+module.exports = averagePair
