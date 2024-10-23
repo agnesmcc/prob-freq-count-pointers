6d4a5fab8dab29d4839c5fc4be662625a4dbe838
Agnes <agnes.mccall5@gmail.com>
2024-10-22 20:37:25 -0400

    add twoArrayObject func

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keysArray, valuesArray) {
+    let obj = {};
+
+    for (let i = 0; i < keysArray.length; i++) {
+        obj[keysArray[i]] = valuesArray[i] || null;
+    }
+
+    return obj;
 }
+
+module.exports = twoArrayObject;
