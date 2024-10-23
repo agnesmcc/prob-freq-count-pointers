43b88a2afa9b3785e37ade174f635e27e4432745
Agnes <agnes.mccall5@gmail.com>
2024-10-22 22:15:06 -0400

    add separatePositive func

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,31 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(intArray) {
+    function swap(arr, i, j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    let start = 0;
+    let end = intArray.length - 1;
+
+    // Iterate until the start index is less than the end index
+    while (start < end) {
+        // If the element at the start index is positive, move the start index forward
+        if (intArray[start] > 0) {
+            start++;
+        // If the element at the end index is negative, move the end index backward
+        } else if (intArray[end] < 0) {
+            end--;
+        // If the element at the start is negative and the element at the end is positive, swap them
+        } else {
+            swap(intArray, start, end);
+            start++;
+            end--;
+        } 
+    }
+
+    return intArray;
 }
+
+module.exports = separatePositive;
