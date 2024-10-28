605562e3b5e2b422e0208f1524153ec42925db4d
Agnes <agnes.mccall5@gmail.com>
2024-10-27 22:02:18 -0400

    add isSubsequence function

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,28 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(string1, string2) {
+
+    if (string1.length > string2.length) {
+        return false;
+    }
+
+    let start = 0;
+    let i = 0;
+    let j = 0;
+    // iterate through string2
+    while (j < string2.length) {
+        // if character in string1 matches character in string2, move forward in string1
+        if (string1[i] === string2[j]) {
+            i++;
+            // if we've reached the end of string1, return true
+            if (i === string1.length) {
+                return true;
+            }
+        }
+        // move forward in string2
+        j++;
+    }
+
+    return false;
 }
+
+exports = module.exports = isSubsequence;
