432f4858ec037e62f5933a138f5f8ee61900b0af
Agnes <agnes.mccall5@gmail.com>
2024-10-28 21:09:30 -0400

    add countPairs func

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(numbers, target) {
+
+    let count = 0;
+
+    let obj = {};
+    // iterate through the array and keep a count of the total number of pairs found
+    // store each number in an object, and check if the difference between the target 
+    // and the current number has been seen before. If it has, increment count.
+    for (let i = 0; i < numbers.length; i++) {
+        let diff = target - numbers[i];
+        if (obj[diff]) {
+            count++;
+        }
+        obj[numbers[i]] = true;
+    }
+
+    return count;
 }
+
+module.exports = countPairs;
